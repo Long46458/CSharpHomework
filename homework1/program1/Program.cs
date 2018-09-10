3c18701f70a510f04ea57040830bf8f2dc10516e
Long46458 <Long46458@163.com>
2018-09-11 00:08:35 +0800

    homework1

---
+++ homework1/program1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            double n = 0;
+            double d = 0;
+            Console.Write("Please input the first number:");
+            s = Console.ReadLine();
+            n = Double.Parse(s);
+            Console.Write("Please input the second number:");
+            s = Console.ReadLine();
+            d = Double.Parse(s);
+            Console.WriteLine("The answer is " + (n*d));
+
+        }
+    }
+}
---
+++ homework1/program2/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            double d = double.Parse(s);
+            string t = textBox2.Text;
+            double n = double.Parse(t);
+            double Product = n * d;
+            label1.Text = "两数的乘积是" + Product;
+
+
+        }
+    }
+}
