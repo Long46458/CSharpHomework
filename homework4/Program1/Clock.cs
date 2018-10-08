2bd525036e30c58f9ce3021a57719d05cae18928
Long46458 <Long46458@163.com>
2018-10-08 20:59:23 +0800

    homework4

---
+++ homework4/Program1/Clock.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    public class ClockEventArgs : EventArgs
+    {
+     public DateTime Time
+        {   set;
+            get;
+        }
+    }
+
+
+    //声明一个委托
+
+    public delegate void ClockEventHandler(object sender, ClockEventArgs e);
+    class Clock
+    {
+        //在委托的机制下我们建立事件
+
+        public event ClockEventHandler OnTime;
+
+
+
+        public void SetAlarm(DateTime time)
+        //如果有人注册了这个事件，也就是这个事件不是空
+
+        {
+            if (OnTime != null)
+            {
+                ClockEventArgs args = new ClockEventArgs();
+                args.Time = time;
+                OnTime(this, args);
+            }
+        }
+    }
+}
---
+++ homework4/Program1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock theClock = new Clock();
+            theClock.OnTime += ItISOnTime;
+            DateTime time = DateTime.Now;
+            bool flag = true;
+            while (flag)
+            {
+                //异常处理
+                try
+                {
+                    Console.Write("Please input the hour:");//时间设定
+                    int clockHour = int.Parse(Console.ReadLine());
+                    Console.Write("Please input the minute:");
+                    int clockMinute = int.Parse(Console.ReadLine());
+                    time = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, clockHour, clockMinute, 0);
+                    if (time < DateTime.Now)
+                    {
+                        Console.WriteLine("wrong");
+                        continue;
+                    }
+                    flag = false;
+                }
+                catch
+                {
+                    Console.WriteLine("wrong");
+                }
+            }
+            theClock.SetAlarm(time);
+            Console.ReadKey();
+        }
+        static void ItISOnTime(object sender, ClockEventArgs e)
+        {
+            while (e.Time > DateTime.Now)
+            {
+                System.Threading.Thread.Sleep(1000);
+            }
+            Console.WriteLine("linglingling！！！");
+            while (true)
+            {
+                Console.Beep();
+            }
+        }
+    }
+}
