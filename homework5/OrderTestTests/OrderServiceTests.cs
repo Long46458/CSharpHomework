099a66b95945deaae9080e5849b53e8d6356a56b
Long46458 <Long46458@163.com>
2018-10-24 12:17:37 +0800

    homework6

---
+++ homework5/OrderTestTests/OrderServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ordertest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ordertest.Tests
+{
+    [TestClass()]
+    public class OrderServiceTests
+    {
+        [TestMethod()]
+        public void QueryByPriceTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
--- homework6/Program1/Properties/AssemblyInfo.cs
+++ homework6/Program1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("program1")]
+[assembly: AssemblyTitle("Program1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("program1")]
+[assembly: AssemblyProduct("Program1")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("db4ba4f3-a9b7-4234-af53-6a8e75973cf9")]
+[assembly: Guid("79605cb9-900c-4dd2-911a-a285c2bb5ceb")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
